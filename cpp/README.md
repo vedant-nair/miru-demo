2e382b902dabdab87db9d0411f68b54a998d7bb3
Benjamin Smidt <ben@miruml.com>
2025-04-30 10:08:39 -0700

    feat(cpp): add directory for getting started with cpp

---
+++ cpp/README.md
@@ -0,0 +1,24 @@
+# Get Started with the Miru C++ SDK
+To begin using the miru sdk, clone this [getting-started](https://github.com/miruml/getting-started) repository and navigate the `/cpp` directory from the root of the repository.
+
+```bash
+git clone https://github.com/miruml/getting-started.git
+```
+```bash
+cd getting-started/cpp
+```
+
+Build the examples (located in the `/cpp/examples` directory).
+
+```bash
+cmake -B build -S .
+```
+```bash
+cmake --build build
+```
+
+Run an example
+
+```bash
+./build/development-example
+```
---
+++ cpp/examples/development/main.cpp
@@ -0,0 +1,20 @@
+// std
+#include <iostream>
+
+// miru
+#include <miru/config/config.hpp>
+#include <miru/query/query.hpp>
+#include <miru/query/ros2.hpp>
+
+int main() {
+    miru::config::Config config = miru::config::Config::from_file(
+        "../schemas/led.schema.yaml",
+        "../configs/led.yaml"
+    );
+    miru::query::ROS2 ros2(config);
+
+    auto params = miru::query::list_params(config);
+    std::cout << params << std::endl;
+
+    std::cout << "ros2 parameter: " << ros2.get_parameter("led.brightness_percent") << std::endl;
+}
